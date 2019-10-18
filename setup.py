276dd215c3cb663721e3a39bf3aef96bcdfe3ac4
unknown <sabbiramin.cse11ruet@gmail.com>
2019-10-18 22:47:19 +0600

    Adding Files

--- setup.py
+++ setup.py
@@ -13,12 +13,19 @@ setup(
     name='mockr',
     author='S.M. Sabbir Amin',
     author_email='<sabbiramin.cse11ruet@gmail.com>,<sabbir@rokomari.com',
-    version='1.1.0',
+    version='0.8.0',
     description='A RESTful Mock Server Generator',
     packages=["mockr"],
     entry_points={"console_scripts": ["mockr = mockr.__main__:main"]},
-    license='MIT',
+    license='GNU GPL v3 or later',
     install_requires=[
     ],
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
